fbf7bc9f71bd798fb1878c0f09b8a5b4babc3046
jspaeth <jon.spaeth@caylent.com>
2026-02-06 13:43:54 -0600

    fix issue

--- src/lib/rate-limiter.ts
+++ src/lib/rate-limiter.ts
@@ -1,14 +1,14 @@
 import { Ratelimit } from "@upstash/ratelimit";
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
 
 export const internalRateLimiter = new Ratelimit({
-  redis: kv,
+  redis: Redis.fromEnv(),
   // 10 requests from the same IP in 10 seconds
   limiter: Ratelimit.slidingWindow(10, "10 s"),
 });
 
 export const externalRateLimiter = new Ratelimit({
-  redis: kv,
+  redis: Redis.fromEnv(),
   // 10 requests from the same IP in 10 seconds
   limiter: Ratelimit.slidingWindow(10, "1 m"),
 });
